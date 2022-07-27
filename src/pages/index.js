a08a174bdf67a08bec41ce3f66bb8c2b2c249406
Yury Torgashin <imaginarylight@yandex.ru>
2022-07-27 22:13:39 +0300

    исправить оставшиеся замечания после ревью

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import "../pages/index.css";
 
-import { Api } from "../components/Xpi.js";
+import { Api } from "../components/Api.js";
 import { UserInfo } from "../components/UserInfo.js";
-import { Card } from "../components/Xard.js";
+import { Card } from "../components/Card.js";
 import { Section } from "../components/Section.js";
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { PopupWithForm } from "../components/PopupWithForm.js";
